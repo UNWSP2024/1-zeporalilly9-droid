3e13e72ef4401a72ee4120a0910f20f901b9db99
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-05 20:02:39 -0500

    PSEO Assignment 1
    week 1 assignment

--- program_1.py
+++ program_1.py
@@ -4,3 +4,8 @@ def hello_world():
     print('Hello ...')
 
 hello_world()
+def Hello_World():
+    # date when program was written 9/5/2025
+    # program title "Hello, World"
+    print("Hello, World")
+
