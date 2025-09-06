177b475096f76f09e825f189466eaa4b869453a1
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-05 20:14:13 -0500

    fixed program 1

--- program_1.py
+++ program_1.py
@@ -1,11 +1,5 @@
-def hello_world():
-    # Update the message below to say "Hello World", 
-    # Python cares about white space, so watch your indentation.
-    print('Hello ...')
-
-hello_world()
 def Hello_World():
     # date when program was written 9/5/2025
-    # program title "Hello, World"
-    print("Hello, World")
+    # program title "Hello World",
+    print('Hello World')
 
